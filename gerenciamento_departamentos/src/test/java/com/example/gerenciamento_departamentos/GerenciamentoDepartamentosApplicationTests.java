20337849c63b67114d50f6338b8b3791662e44af
rafaellafanti <rafaellafantidealmeida@gmail.com>
2026-03-22 19:52:04 -0300

    feat: criação da entidade DepartamentoModel

---
+++ gerenciamento_departamentos/src/test/java/com/example/gerenciamento_departamentos/GerenciamentoDepartamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamento_departamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDepartamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
